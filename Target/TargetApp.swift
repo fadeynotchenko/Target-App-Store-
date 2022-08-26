e7a521638c09323f6123d216e97e4bb82dc3c4ac
Fadey <notchenkofaddey@gmail.com>
2022-08-26 18:57:20 +0300

    Initial Commit

---
+++ Target/TargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetApp.swift
+//  Target
+//
+//  Created by Fadey Notchenko on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
